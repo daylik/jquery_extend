7e80802e300061e882787e09773840aa3db20642
Олег Мешаев <dayll@yandex.ru>
2016-11-08 16:36:18 +0300

    Update magnific_popup_fullsize.js

--- for - magnific popup/magnific_popup_fullsize.js
+++ for - magnific popup/magnific_popup_fullsize.js
@@ -47,7 +47,7 @@ $.extend(true, $.magnificPopup.defaults, {
 				$('.mfp_go_full_img').fadeIn(300);
 			}
 			
-		  if(window.imgIsClick == 0){
+		  if(window.imgIsClick === 0){
 			$(this).stop("true", "true").animate({'max-height' : img_h+'px'}, 800);
 			$('.mfp_go_full_img').html(window.txtCloseBtn);
 			window.imgIsClick = 1;
